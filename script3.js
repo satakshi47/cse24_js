6db0cc77de0ad74b659665e438819a783d16e749
satakshi47 <srivastavasatakshi47@gmail.com>
2026-02-26 02:09:20 +0530

    comparison operators

---
+++ script3.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 20;
+
+// a == b
+console.log("a == b = " + (a == b));
+
+// a < b
+console.log("a < b = " + (a < b));
+
+// a > b
+console.log("a > b = " + (a > b));
+
+// a != b
+console.log("a != b =" + (a != b));
+
+// a >= b
+console.log("a >= b = " + (a >= b));
+
+// a <= b
+console.log("a <= b = " + (a <= b));
