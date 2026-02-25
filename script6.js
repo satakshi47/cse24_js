bb5a533478759deb97ca6e7b6eb407074d6c451c
satakshi47 <srivastavasatakshi47@gmail.com>
2026-02-26 02:19:47 +0530

    arithmeic operators

---
+++ script6.js
@@ -0,0 +1,19 @@
+// Arithmetic Operators 
+
+var a = 40;
+var b = 20;
+
+// a = a + b
+console.log("Value of a = (a + b) = " + (a + b));
+
+// a = a - b
+console.log("Value of a = (a - b) = " + (a - b));
+
+// a = a * b
+console.log("Value of a = (a * b) = " + (a * b));
+
+// a = a / b
+console.log("Value of a = (a / b) = " + (a / b));
+
+// a = a % b
+console.log("Value of a = (a % b) = " + (a % b));
